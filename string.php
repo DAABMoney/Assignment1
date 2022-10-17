d0d672b17debb653535d97a92574051d80269288
Duane <duane.brk@gmail.com>
2022-10-16 20:01:22 -0500

    Typo fix

--- string.php
+++ string.php
@@ -11,14 +11,14 @@ include 'includes/header.php';
     <?php 
    $phrase = "PHP is cool";
    $phrase = "PHP is a server side script";
-   $name = "Duane";
+   $name = " Duane ";
    echo '<br/>';
    echo '<br/>';
 
-   echo 'Uppercase first letter:' . ucfirst($name).'<br/>';
-   echo 'Uppercase first letter of each word' . ucwords($name).'<br/>';
-   echo 'Repeat String:' . str_repeat('d', 4) . '<br/>';
-   echo 'Repeat String - Nested Function:' . strtoupper(str_repeat('a',5)) . '<br/>'; 
+   echo 'Uppercase first letter:  ' . ucfirst($name).'<br/>';
+   echo 'Uppercase first letter of each word  ' . ucwords($name).'<br/>';
+   echo 'Repeat String:  ' . str_repeat('d', 4) . '<br/>';
+   echo 'Repeat String - Nested Function:  ' . strtoupper(str_repeat('a',5)) . '<br/>'; 
     ?>
 
 </div>
